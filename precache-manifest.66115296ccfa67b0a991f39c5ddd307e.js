0a3d9b92c0f2bda5fd955ef500d9ce6e1f35ea0e
Dextrous <ssjdexucyche@gmail.com>
2019-11-08 18:56:04 +0530

    Updates

---
+++ precache-manifest.66115296ccfa67b0a991f39c5ddd307e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69fdddd2cc2fc5a7ec7718548cb6da4c",
+    "url": "/moviesearch/index.html"
+  },
+  {
+    "revision": "4ec7a0de48d309f2f25c",
+    "url": "/moviesearch/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "fa17c0b47ef9e3cf40cc",
+    "url": "/moviesearch/static/css/main.46738881.chunk.css"
+  },
+  {
+    "revision": "4ec7a0de48d309f2f25c",
+    "url": "/moviesearch/static/js/2.493bf450.chunk.js"
+  },
+  {
+    "revision": "fa17c0b47ef9e3cf40cc",
+    "url": "/moviesearch/static/js/main.0437470e.chunk.js"
+  },
+  {
+    "revision": "4570e7737edc5a3cc6e8",
+    "url": "/moviesearch/static/js/runtime-main.0ef4d193.js"
+  }
+]);
